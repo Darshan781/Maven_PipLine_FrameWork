329be919bf33589005face67eee4904bdc4fe448
Darshan <darshankv789raaga@gmail.com>
2026-07-09 08:48:23 +0530

    AutoTrigger

---
+++ src/test/java/com/demoapps/testcase/FuncationalTestCase1.java
@@ -0,0 +1,32 @@
+package com.demoapps.testcase;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.demoapps.TestData.TestDataProvider;
+import com.demoapps.testBase.BaseTest;
+import com.demoappsframwork.PageLayers.RegisterPage;
+import com.demoappsframwork.PageLayers.UserPage;
+
+public class FuncationalTestCase1 extends BaseTest {
+	 UserPage userPage;
+	 RegisterPage registerPage;
+	 	 
+	 
+//	 @BeforeMethod
+//	 public void getInToRegisterPage() {
+//		 
+//		
+//	}
+//	 
+	 @Test(dataProvider = "userRegData",dataProviderClass = TestDataProvider.class)
+	    public void FC_01userRegistrationPage(String name,String lname,String email,String pwd,String gender) {
+		 registerPage =	welcomePage.clickOnRegisterLink();
+	     userPage =  registerPage.doRegister(name, lname, email, pwd, gender);
+	     //Assert.fail();
+		 userPage.doLogout();
+		 welcomePage.clickOnRegisterLink();
+		}
+}
